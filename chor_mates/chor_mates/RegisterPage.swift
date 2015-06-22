566eee179aac2f7da1b3050b2915ead6dbd6fb2a
pmoise <pam416@nyu.edu>
2015-06-21 22:12:28 -0400

    Login/Register Pages

---
+++ chor_mates/chor_mates/RegisterPage.swift
@@ -0,0 +1,9 @@
+//
+//  RegisterPage.swift
+//  chor_mates
+//
+//  Created by Philip Moise on 6/20/15.
+//  Copyright (c) 2015 ChorMates. All rights reserved.
+//
+
+import Foundation
---
+++ chor_mates/chor_mates/ViewTextController.swift
@@ -0,0 +1,9 @@
+//
+//  TextField.swift
+//  chor_mates
+//
+//  Created by Philip Moise on 6/21/15.
+//  Copyright (c) 2015 ChorMates. All rights reserved.
+//
+
+import Foundation
