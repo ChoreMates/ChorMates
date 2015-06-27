0139d2e861d6b7c27b0f99e769ec62c8129d5abd
kpapa967 <kp967@nyu.edu>
2015-06-26 21:08:56 -0400

    added expenses view controller, and segmented controller

---
+++ chor_mates/chor_mates/Expenses.swift
@@ -0,0 +1,12 @@
+//
+//  Expenses.swift
+//  chor_mates
+//
+//  Created by Kostaq Papa on 6/26/15.
+//  Copyright (c) 2015 ChorMates. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class Expenses: UIViewController {}
