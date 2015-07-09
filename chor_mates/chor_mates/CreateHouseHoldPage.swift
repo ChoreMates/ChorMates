59e6eae5ad38130c14c90fb3ba09b474b2e7aa47
pmoise <pam416@nyu.edu>
2015-07-08 22:12:35 -0400

    Join Household Page

--- chor_mates/chor_mates/CreateHouseHoldPage.swift
+++ chor_mates/chor_mates/CreateHouseHoldPage.swift
@@ -24,7 +24,8 @@ class CreateHouseHoldPage: ViewTextController {
         //logoutButton.setTitleTextAttributes([NSFontAttributeName: UIFont(name: "Noteworthy", size: 20)!], forState: UIControlState.Normal)
         self.navigationItem.rightBarButtonItem = logoutButton
         
-        //Check if request, if yes make sure if want to continue. If yes, send email to household owner saying no more request, remove from table his request
+        // Check if previously requested, if yes make sure if want to continue.
+        // If yes, send email to household owner saying no more request, remove request from the table
     }
     
     override func DismissKeyboard() {
@@ -34,7 +35,7 @@ class CreateHouseHoldPage: ViewTextController {
     
     func LogOut() {
         PFUser.logOut()
-        performSegueWithIdentifier("HHToLogin", sender: nil)
+        performSegueWithIdentifier("CreateToLogin", sender: nil)
     }
     
     @IBAction func HomeSweetHome(sender: UIButton) {
@@ -46,15 +47,6 @@ class CreateHouseHoldPage: ViewTextController {
             var address: String = address1.text
             if(!address2.text.isEmpty) {
                 address += ", \(address2.text)"
-            }
-            
-            println(householdName.text)
-            println(address)
-            if(privacy.on) {
-                println("It's on!")
-            }
-            else {
-                println("It's off!")
             }
         
             var household = PFObject(className: "Household")
@@ -75,20 +67,27 @@ class CreateHouseHoldPage: ViewTextController {
                             //Redirect to home page
                         }
                         else {
-                            println("Problem, Uh-oh!!")
-                            //if problem reaches here, delete household and tell user to retry
+                            household.deleteInBackgroundWithBlock {
+                                (success: Bool, error: NSError?) -> Void in
+                                if (!success) {
+                                    println("Delete failed? \(error!.description)")
+                                    //Assuming the connection loss is the cause of the failure
+                                    household.deleteEventually()
+                                }
+                                self.PopUp("Household Not Created", image: nil, msg: "The household could not be created!\nPlease try again.", animate: true,  onCloseFunc: nil)
+                            }
                         }
                     }
                 }
                 else {
-                    println("Problem!!")
-                    //retry creating the household
+                    self.PopUp("Household Not Created", image: nil, msg: "The household could not be created!\nPlease try again.", animate: true,  onCloseFunc: nil)
                 }
             }
         }
     }
     
     func RedirectHomePage() {
+        PFUser.logOut() //Temp! Only until tab view is working
         let controller = storyboard?.instantiateViewControllerWithIdentifier("MyChoresPage") as! UITableViewController
         presentViewController(controller, animated: true, completion: nil)
     }
--- chor_mates/chor_mates/JoinHouseholdPage.swift
+++ chor_mates/chor_mates/JoinHouseholdPage.swift
@@ -31,6 +31,11 @@ class JoinHouseholdPage: ViewTextController {
         }
     }
     
+    func LogOut() {
+        PFUser.logOut()
+        performSegueWithIdentifier("JoinToLogin", sender: nil)
+    }
+    
     func GoBack() {
         navigationController?.popViewControllerAnimated(true)
     }
@@ -44,59 +49,67 @@ class JoinHouseholdPage: ViewTextController {
                 PopUp("E-Mail Error", image: nil, msg: "You can't join your own household!", animate: true)
             }
             else {
-                //Query for the requested user
-                var query = PFUser.query()
-                query!.whereKey("email", equalTo:email.text)
-                query?.findObjectsInBackgroundWithBlock {
-                    (users: [AnyObject]?, error: NSError?) -> Void in
+                //Find household of user based on email address
+                var query = PFQuery(className: "Household_User")
+                var inner = PFUser.query()!
+                inner.whereKey("email", equalTo:email.text)
+                query.whereKey("userID", matchesQuery: inner)
+                query.findObjectsInBackgroundWithBlock {
+                    (usersHH: [AnyObject]?, error: NSError?) -> Void in
                     if error == nil {
-                        if(users!.count == 0) {
-                            self.PopUp("Request Not Sent", image: nil, msg: "E-Mail address is not found!", animate: true,  onCloseFunc: nil)
+                        if(usersHH!.count == 0) {
+                            self.PopUp("Email Error", image: nil, msg: "Could not find the household for the email address provided!", animate: true,  onCloseFunc: nil)
                         }
                         else {
-                            if let users = users as? [PFObject] {
-                                var user = users.first!
-                                println(user.objectId!)
-                                // Query for the requested user's household
-                                var query2 = PFQuery(className: "Household_User")
-                                query2.whereKey("userID", equalTo: user)
-                                query2.findObjectsInBackgroundWithBlock {
-                                    (objects: [AnyObject]?, error2: NSError?) -> Void in
-                                    if error2 == nil {
-                                        if(objects!.count == 0) {
-                                            self.PopUp("Request Not Sent", image: nil, msg: "User is not part of a household!", animate: true,  onCloseFunc: nil)
-                                        }
-                                        else {
-                                            if let hHolds = objects as? [PFObject] {
-                                                var hHold = hHolds.first!
-                                                println(hHold["householdID"]!.objectId!!)
-                                                
-                                                //Create HouseInvitation object and send email to household owner
-                                                var houseInvite = PFObject(className: "HouseInvitation")
-                                                houseInvite["userID"] = PFUser.currentUser()
-                                                houseInvite["householdID"] = hHold["householdID"]!
-                                                houseInvite["status"] = false
-                                                houseInvite.saveInBackgroundWithBlock {
-                                                    (success: Bool, error3: NSError?) -> Void in
-                                                    if (success) {
-                                                        self.PopUp("Request Sent", image: nil, msg: "You're request to join has been sent!", animate: true,  onCloseFunc: nil)
-                                                    }
-                                                    else {
-                                                        self.PopUp("Request Not Sent", image: nil, msg: "Could not send request!", animate: true,  onCloseFunc: nil)
-                                                        println("Error3: \(error3!) \(error3!.userInfo!)")
-                                                    }
-                                                }
-                                            }
-                                        }
+                            if let usersHH = usersHH as? [PFObject] {
+                                var userHH = usersHH.first!
+                                //Create HouseInvitation object and send email to household owner
+                                var houseInvite = PFObject(className: "HouseInvitation")
+                                houseInvite["userID"] = PFUser.currentUser()
+                                houseInvite["householdID"] = userHH["householdID"]!
+                                houseInvite["status"] = false
+                                houseInvite.saveInBackgroundWithBlock {
+                                    (success: Bool, error2: NSError?) -> Void in
+                                    if (success) {
+                                        self.PopUp("Request Sent", image: nil, msg: "You're request to join has been sent!", animate: true,  onCloseFunc: nil)
+                                        
+                                        //Send email to household owner, query for hh owner
+                                        // DOESN'T WORK YET! Successfully gets email, no email sent
+//                                        var queryHH = PFQuery(className: "Household")
+//                                        println(userHH["householdID"]!.objectId!!)
+//                                        queryHH.whereKey("objectId", equalTo: userHH["householdID"]!.objectId!!)
+//                                        queryHH.includeKey("createdBy")
+//                                        queryHH.findObjectsInBackgroundWithBlock {
+//                                            (houseHold: [AnyObject]?, error: NSError?) -> Void in
+//                                            if error == nil {
+//                                                if houseHold!.count == 0 {
+//                                                    println("No household by that id!")
+//                                                }
+//                                                else {
+//                                                    if let houseHold = houseHold as? [PFObject] {
+//                                                        let hh = houseHold.first!
+//                                                        println("Found the household!")
+//                                                        let creator = hh["createdBy"]! as! PFUser
+//                                                        println(creator.email!)
+//                                                    }
+//                                                }
+//                                            }
+//                                            else {
+//                                                println("Error: \(error!) \(error!.userInfo!)")
+//                                            }
+//                                        }
+                                        //End send email query
                                     }
                                     else {
-                                        println("Error2: \(error2!) \(error2!.userInfo!)")
+                                        self.PopUp("Request Not Sent", image: nil, msg: "Could not send request!\nPlease try again.", animate: true,  onCloseFunc: nil)
+                                        println("Error3: \(error2!) \(error2!.userInfo!)")
                                     }
                                 }
                             }
                         }
                     }
                     else {
+                        self.PopUp("Request Not Sent", image: nil, msg: "Request could not be sent!\nPlease try again.", animate: true)
                         println("Error: \(error!) \(error!.userInfo!)")
                     }
                 }
